bc61fabafa7e9e048b71411681571c9dc713c9b6
Mike English <mike.english@atomicobject.com>
2013-10-21 16:47:15 -0400

    Update project repo remote.

--- chef/site-cookbooks/aerostat/attributes/default.rb
+++ chef/site-cookbooks/aerostat/attributes/default.rb
@@ -3,7 +3,7 @@ node.default["aerostat"]["pilot"]["github_username"] = "englishm"
 node.default["aerostat"]["copilot"]["username"] = "copilot"
 node.default["aerostat"]["gitconfig"]["email"] = "mike.english@gmail.com"
 node.default["aerostat"]["gitconfig"]["name"] = "Mike English"
-node.default["aerostat"]["repo_url"] = "git@gitlab.atomicobject.com:mike.english/aerostat.git"
+node.default["aerostat"]["repo_url"] = "git@github.com:englishm/aerostat.git"
 node.default["aerostat"]["fqdn"] = "aerostat.mikeenglish.net"
 node.default["aerostat"]["vimconfig_repo_url"] = "git@gitorious.atomicobject.com:vim-settings/colthorp.git"
 
