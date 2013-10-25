1165dc14489d0e51714c861054ecf9cd70fe2feb
Mike English <mike.english@gmail.com>
2013-10-25 20:21:48 +0000

    Fix Vagrant plugins.

--- Berksfile
+++ Berksfile
@@ -6,4 +6,4 @@ site :opscode
 cookbook 'rbenv', git: 'git://github.com/RiotGames/rbenv-cookbook', ref: '1.6.5'
 cookbook 'sudo', git: 'git://github.com/opscode-cookbooks/sudo', ref: 'v2.2.2'
 cookbook 'python', git: 'git://github.com/opscode-cookbooks/python', ref: 'v1.4.0'
-cookbook 'vagrant', git: 'git://github.com/jtimberman/vagrant-cookbook', ref: '0.2.0'
+cookbook 'vagrant', git: 'git://github.com/oker1/vagrant-cookbook', ref: 'plugin_user'
--- chef/site-cookbooks/aerostat/recipes/vagrant.rb
+++ chef/site-cookbooks/aerostat/recipes/vagrant.rb
@@ -2,11 +2,18 @@
 
 node.default['vagrant']['url'] = "http://files.vagrantup.com/packages/a40522f5fabccb9ddabad03d836e120ff5d14093/vagrant_1.3.5_x86_64.deb"
 node.default['vagrant']['checksum'] = "db7d06f46e801523d97b6e344ea0e4fe942f630cc20ab1706e4c996872f8cd71"
+node.default['vagrant']['checksum'] = "db7d06f46e801523d97b6e344ea0e4fe942f630cc20ab1706e4c996872f8cd71"
 
 include_recipe "vagrant"
 
 
-# TODO: find out why these are installed to root user only.
-vagrant_plugin "vagrant-omnibus"
-vagrant_plugin "vagrant-aws"
-vagrant_plugin "vagrant-digitalocean"
+# TODO: switch back to main vagrant cookbook repo when PR merged
+vagrant_plugin "vagrant-omnibus" do
+  user $pilot_username
+end
+vagrant_plugin "vagrant-aws" do
+  user $pilot_username
+end
+vagrant_plugin "vagrant-digitalocean" do
+  user $pilot_username
+end
