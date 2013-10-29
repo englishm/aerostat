f4efcc865e7d8df4084e8df6de4797f7807dd658
Mike English <mike.english@gmail.com>
2013-10-29 05:43:49 +0000

    Add basic guard to Valgrind resource.

--- chef/site-cookbooks/aerostat/recipes/proglang_c.rb
+++ chef/site-cookbooks/aerostat/recipes/proglang_c.rb
@@ -5,4 +5,5 @@ package 'libc6-dbg'
 ark 'valgrind' do
   url 'http://valgrind.org/downloads/valgrind-3.8.1.tar.bz2'
   action :install_with_make
+  not_if do ::File.exists?('/usr/local/bin/valgrind') end
 end
