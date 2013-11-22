02602e7dae59e6132931341a6ac59548a8b349f4
Mike English <mike.english@gmail.com>
2013-11-22 15:02:01 +0000

    Update Packer to v0.4.0.

--- chef/site-cookbooks/aerostat/recipes/packer.rb
+++ chef/site-cookbooks/aerostat/recipes/packer.rb
@@ -1,7 +1,7 @@
 # Packer
 
 ark 'packer' do
-  url 'https://dl.bintray.com/mitchellh/packer/0.3.10_linux_amd64.zip'
+  url 'https://dl.bintray.com/mitchellh/packer/0.4.0_linux_amd64.zip'
   action :dump
   path '/usr/local/bin'
   creates '/usr/local/bin/packer'
